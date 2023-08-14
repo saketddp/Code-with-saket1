d3da9725302ab35496aec818a5ed141575b25362
saketddp <129578803+saketddp@users.noreply.github.com>
2023-08-14 22:49:26 +0530

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -1,1 +1,12 @@
+//print 1 to 10 using while loop
 
+#include <iostream>  
+using namespace std;  
+int main() {         
+ int i=1;      
+         while(i<=10)   
+       {      
+            cout<<i <<"\n";    
+            i++;  
+          }       
+    }  
